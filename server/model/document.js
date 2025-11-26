96f4c286105b9796956f5a76be26bfe49db9149f
Amulya2206 <yathipathianila@gmail.com>
2025-11-26 19:08:37 +0530

    document.js

---
+++ server/model/document.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+const DocumentSchema = new mongoose.Schema({
+docId: { type: String, required: true, unique: true },
+content: { type: String, default: '' },
+updatedAt: { type: Date, default: Date.now }
+});
+
+
+module.exports = mongoose.model('Document', DocumentSchema);
